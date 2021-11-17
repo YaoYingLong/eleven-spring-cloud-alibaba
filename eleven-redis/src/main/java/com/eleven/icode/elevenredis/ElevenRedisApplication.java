24600618073c903155c47da7f77a9165297a5999
yao_yinglong <yao_yinglong@icekredit.com>
2021-11-17 09:32:27 +0800

    feat：补充Redis用例

---
+++ eleven-redis/src/main/java/com/eleven/icode/elevenredis/ElevenRedisApplication.java
@@ -0,0 +1,16 @@
+package com.eleven.icode.elevenredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by YingLong on 2021/11/16
+ */
+@SpringBootApplication
+public class ElevenRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevenRedisApplication.class, args);
+    }
+
+}
