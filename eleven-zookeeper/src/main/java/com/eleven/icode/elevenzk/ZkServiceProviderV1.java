637707741861b9cc76f08ec8e2f98cac87c9c396
yao_yinglong <yao_yinglong@icekredit.com>
2021-12-21 18:01:26 +0800

    feat：添加Zookeeper用例2

--- eleven-zookeeper/src/main/java/com/eleven/icode/elevenzk/ZkServiceProviderV1.java
+++ eleven-zookeeper/src/main/java/com/eleven/icode/elevenzk/ZkServiceProviderV1.java
@@ -9,20 +9,16 @@ import java.util.concurrent.CountDownLatch;
  * @author by Eleven on 2021/12/20
  */
 public class ZkServiceProviderV1 implements Watcher {
-
     private static CountDownLatch countDownLatch = new CountDownLatch(1);
     private static ZooKeeper zk = null;
     private static String rootPath = "/GroupMembers";
-
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         zk = new ZooKeeper("localhost:2181", 5000, new ZkServiceProviderV1());
         countDownLatch.await();
-        zk.create(rootPath + "/c1", "test carete".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        zk.create(rootPath + "/test", "test carete".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
         System.out.println("创建集群节点c1：" + rootPath + "c1");
         Thread.sleep(Integer.MAX_VALUE);
     }
-
-
     @Override
     public void process(WatchedEvent event) {
         if (Event.KeeperState.SyncConnected == event.getState()) {
